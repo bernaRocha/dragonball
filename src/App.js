fbff6dbfaa55570c60c37dd0a7c9cf2bf2e2e872
bernaRocha <mrochabernardo@gmail.com>
2021-06-16 12:34:00 -0300

    Começo do trabalho

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import logo from './logo.png'
+
+function App() {
+  return (
+    <>
+      <div className='logoBar'>
+        <img className='logoImg' src={logo} alt='Logo do anime Dragon Ball Z' />
+      </div>
+    </>    
+  );
+}
+
+export default App;
