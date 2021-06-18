7c81cbbe8d77f1a2fee750529207a9aaaaaa1b21
bernaRocha <mrochabernardo@gmail.com>
2021-06-17 23:01:33 -0300

    Segunda edição

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import './App.scss';
-import logo from './logo.png'
+
+// Components
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
+import { Switch, Route } from 'react-router-dom'
+import { ReadAll } from './Components/ReadAll/Readall'
+import { Create } from './Components/Create/Create'
+import { DeleteAll } from './Components/DeleteAll/Deletall'
+import { About } from './Components/About/About'
+
+// Assets
+import logo from './Img/logo.png'
+
+// Styles 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Styles/App.scss';
+
 
 function App() {
   return (
@@ -8,6 +22,32 @@ function App() {
       <div className='logoBar'>
         <img className='logoImg' src={logo} alt='Logo do anime Dragon Ball Z' />
       </div>
+
+    
+      <Navbar className='justify-content-center' expand='sm'>
+        <Nav>
+          <Nav.link id='link-style' href='/'>Início</Nav.link>
+          <Nav.link id='link-style' href='/create'>Criar</Nav.link>
+          <Nav.link id='link-style' href='/delete-all'>Deletar tudo</Nav.link>
+          <Nav.link id='link-style' href='/about'>Sobre</Nav.link>
+          
+          
+        </Nav>
+      </Navbar>
+
+      <Container className='app-container'>
+        <Row>
+          <Col>
+            <Switch>
+              <Route path='/' exact={true} component={ReadAll}></Route>
+              <Route path='/create' component={Create}></Route>
+              <Route path='/delete-all' component={DeleteAll}></Route>
+              <Route path='/about' component={About}></Route>
+            </Switch>
+
+          </Col>
+        </Row>
+      </Container>
     </>    
   );
 }
